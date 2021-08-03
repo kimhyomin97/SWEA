4d293dac6ead7a59e7d11eba3737d9915bcd8e90
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-03 13:39:29 +0900

    08/03

--- 쩜/퇴사.cpp
+++ 쩜/퇴사.cpp
@@ -1,4 +1,4 @@
-// 백준 14501 퇴사
+// 백준 14501 퇴사 // 다시 
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -8,18 +8,26 @@ int t[16];
 int p[16];
 int dp[16];
 
-int res = 0;
+int ans = 0;
 
 int main(){
 	scanf("%d", &n);
 	for(int i=0; i<n; i++){
-		scanf("%d", t[i]);
-		scanf("%d", p[i]);
+		scanf("%d", &t[i]);
+		scanf("%d", &p[i]);
 	}
 	
-	for(int i=0; i<n; i++){
-		
+	for(int i=n-1; i>=0; i--){
+		// 뒤부터 계산하는 방법
+		if(t[i] + i > n) dp[i] = dp[i+1];
+		else{
+			dp[i] = max(dp[i+t[i]]+p[i], dp[i+1]);
+		}
 	}
+//	for(int i=0; i<n; i++){
+//		printf("%d\n", dp[i]);
+//	}
+	printf("%d", dp[0]);
 	
 	return 0;
 } 
