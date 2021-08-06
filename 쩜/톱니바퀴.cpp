2800e7725cca70caa017e46cf2fc9b931beb2b4c
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-06 14:16:27 +0900

    08/06

--- 쩜/톱니바퀴.cpp
+++ 쩜/톱니바퀴.cpp
@@ -6,7 +6,6 @@ int k;
 int ans = 0;
 
 void spin(int num){
-//	num--;
 	// 시계방향 회전 
 	int temp = gear[num][7];
 	for(int i=7; i>0; i--){
@@ -16,7 +15,6 @@ void spin(int num){
 }
 
 void spin_reverse(int num){
-//	num--;
 	int temp = gear[num][0];
 	for(int i=0; i<7; i++){
 		gear[num][i] = gear[num][i+1];
@@ -38,7 +36,6 @@ int main(){
 		if(gear[0][2] != gear[1][6]) connect[0] = true;
 		if(gear[1][2] != gear[2][6]) connect[1] = true;
 		if(gear[2][2] != gear[3][6]) connect[2] = true;
-//		printf("connect : %d %d %d \n", connect[0], connect[1], connect[2]);
 		if(dir == 1){
 			spin(num-1);
 			int cnt = 0;
@@ -79,7 +76,6 @@ int main(){
 			}
 			cnt = 0;
 			for(int i=num-1; i>0; i--){
-//				printf("HEllo\n");
 				if(connect[i-1]){
 					if(cnt % 2 == 0) spin(i-1);
 					else if(cnt%2 == 1) spin_reverse(i-1);
@@ -91,34 +87,7 @@ int main(){
 			}
 		}
 		for(int i=0; i<4; i++) connect[i] = false;
-//		printf("\n");
-//		for(int i=0; i<4; i++){
-//			for(int j=0; j<8; j++){
-//				printf(" %d ", gear[i][j]);
-//			}
-//			printf("\n");
-//		}
-//		printf("\n");
 		}
-
-//	printf("\n");
-//	for(int i=0; i<4; i++){
-//		for(int j=0; j<8; j++){
-//			printf(" %d ", gear[i][j]);
-//		}
-//		printf("\n");
-//	}
-//	spin(0);
-//	printf("reverse : 3 \n");
-//	spin_reverse(3);
-//	printf("\n");
-//	for(int i=0; i<4; i++){
-//		for(int j=0; j<8; j++){
-//			printf(" %d ", gear[i][j]);
-//		}
-//		printf("\n");
-//	}
-//	printf("\n");
 
 	if(gear[0][0] == 1) ans +=1;
 	if(gear[1][0] == 1) ans +=2;
