e401dd85d690a52b5273e5dfb146d3e62cfb9ce3
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-14 23:03:21 +0900

    08/14

---
+++ 쩜/인구이동.cpp
@@ -0,0 +1,23 @@
+// 백준 16234 인구 이동 test
+#include <cstdio>
+int n, l, r;
+int map[50][50];
+int p[50][50];
+int ans = 0;
+
+void solve(){
+    while(1){
+
+    }
+}
+
+int main(){
+    scanf("%d%d%D", &n, &l, &r);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &map[i][j]);
+        }
+    }
+    solve();
+    return 0;
+}
