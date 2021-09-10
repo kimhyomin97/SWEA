2dfa28abdf5273fa0873177a9aa702c9c8afbb3b
kimhyomin97 <rlagyalsdlek@naver.com>
2021-09-10 20:33:25 +0900

    practice folder

--- 쩜/미세먼지안녕!.cpp
+++ 쩜/미세먼지안녕!.cpp
@@ -1,7 +1,19 @@
 // 백준 17144 미세먼지 안녕!
 #include <cstdio>
 
+int r, c, t;
+int map[50][50];
+int ans = 0;
+
 int main(){
-	printf("Hello");
+	scanf("%d%d%d", &r, &c, &t);
+	for(int i=0; i<r; i++){
+		for(int j=0; j<c; j++){
+			scanf("%d", map[i][j]);
+		}
+	}
+	
+	
+	
 	return 0;
 } 
