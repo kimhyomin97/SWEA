b1b10670524b8fbcb9a4c8a13701a6d500218181
kimhyomin97 <rlagyalsdlek@naver.com>
2021-11-21 23:40:44 +0900

    11/21

---
+++ 연습/문자열게임2.cpp
@@ -0,0 +1,42 @@
+// boj 20437 문자열 게임2
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+	int T;
+	scanf("%d", &T);
+	
+	for(int i=0; i<T; i++){
+		string str;
+		int k;
+		char temp[10001];
+//		int cnt[26] = {0,};
+		vector <int> cnt[26];
+		scanf("%s", &temp);
+		str = temp;
+		scanf("%d", &k);
+		int min_ans=10001, max_ans=-1;
+		for(int j=0; str[j]; j++){
+			cnt[str[j]-97].push_back(j);
+		}
+		for(int j=0; j<26; j++){
+			int min=10001, min2=10001, max=-1;
+			if(cnt[j].size() >= k){
+				for(int s=0; s<cnt[j].size(); s++){
+					if(cnt[j][s] < min) min = cnt[j][s];
+					else if(cnt[j][s] < min2) min2 = cnt[j][s];
+					if(cnt[j][s] > max) max = cnt[j][s];
+				}
+			}
+			if((min2 - min) != 0 && min2-min < min_ans) min_ans = min2-min;
+			if(max-min > max_ans) max_ans = max-min;
+		}
+		if(max_ans == -1) printf("-1\n");
+		else
+			printf("%d %d\n", min_ans+1, max_ans);
+	}
+	
+	return 0;
+} 
