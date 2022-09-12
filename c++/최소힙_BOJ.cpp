249f8859635a4c40e9c656e0219957eb4b4f75cb
kimhyomin97 <rlagyalsdlek@naver.com>
2022-09-12 22:41:28 +0900

    BOJ Data Structure

---
+++ c++/최소힙_BOJ.cpp
@@ -0,0 +1,34 @@
+// BOJ 1927 √÷º“»¸
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	priority_queue<int, vector<int>, greater<int> > pq;
+	int n;
+	cin >> n;
+	
+	for(int i=0; i<n; i++){
+		int x;
+		cin >> x;
+		if(x == 0){
+			if(pq.size() == 0){
+				cout << 0 << "\n";
+			}
+			else{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else{
+			pq.push(x);
+		}
+	}
+	
+	
+	return 0;
+} 
