d919249fa40e439bf585dfa17bc0917ec0e38787
kimhyomin97 <rlagyalsdlek@naver.com>
2022-09-21 23:36:05 +0900

    BOJ DP

---
+++ c++/가장긴증가하는부분수열4_BOJ.cpp
@@ -0,0 +1,23 @@
+// BOJ 14002 가장긴증가하는부분수열4
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n; // 1 <= n <= 1000
+	cin >> n;
+	
+	int list[n];
+	int dp[n];
+	
+	for(int i=0; i<n; i++){
+		cin >> list[i];
+		
+	}	
+	
+	return 0;
+}
+
