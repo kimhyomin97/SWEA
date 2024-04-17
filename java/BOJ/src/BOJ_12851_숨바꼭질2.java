ae119a120e4c6c8768d81404fc4ed89110df80c4
kimhyomin97 <rlagyalsdlek@naver.com>
2024-04-17 23:18:38 +0900

    BOJ BFS 응용 (visit 배열 응용) + 시간단축

--- java/BOJ/src/BOJ_12851_숨바꼭질2.java
+++ java/BOJ/src/BOJ_12851_숨바꼭질2.java
@@ -1,8 +1,8 @@
-// 7%에서 틀림 -> visit 처리를 해줘야 할듯
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class BOJ_12851_숨바꼭질2 {
 	public static void main(String[] args) throws Exception {
@@ -38,7 +38,7 @@ public class BOJ_12851_숨바꼭질2 {
 					if(list[now-1] == 0 || list[now-1] >= second) {
 						list[now-1] = second;
 						arr.add(now-1);
-					}	
+					}
 				}catch(Exception e) {}
 				try {
 					if(list[now*2] == 0 || list[now*2] >= second) {
@@ -49,6 +49,55 @@ public class BOJ_12851_숨바꼭질2 {
 			}
 		}
 		list[n] = 0;
-		System.out.println(cnt + " " + list[k]);
+		System.out.println(list[k] + "\n" + cnt);
+	}
+//	아래 코드도 정답, 시간 단축 648ms -> 192ms
+	public static void sol() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken()); // 0 <= k <= 100000
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(n);
+		int ans = 0;
+		int ans_cnt = 0;
+		int[] visit = new int[150001];
+		if(n == k){
+			System.out.println(0);
+			System.out.println(1);
+			return;
+		}
+
+		while(!queue.isEmpty()){
+			int size = queue.size();
+			ans++;
+			for(int i=0; i<size; i++){
+				int now = queue.poll();
+				int nPosition;
+				for(int j=0; j<3; j++){
+					if(j==0){
+						nPosition = now + 1;
+					}
+					else if(j==1){
+						nPosition = now - 1;
+					}
+					else {
+						nPosition = now * 2;
+					}
+					if(nPosition == k){
+						ans_cnt++;
+					}
+					if(nPosition < 0 || nPosition >= 150001 || (visit[nPosition] < ans && visit[nPosition] > 0)){
+						continue;
+					}
+					visit[nPosition] = ans;
+					queue.add(nPosition);
+				}
+			}
+			if(ans_cnt > 0) break;
+		}
+		System.out.println(ans);
+		System.out.println(ans_cnt);
 	}
 }
