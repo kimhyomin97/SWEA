38f367924d67dd8c96c9107b4c0dc313725b4edd
kimhyomin97 <rlagyalsdlek@naver.com>
2025-01-12 00:26:04 +0900

    BOJ 이분탐색 (lower bound, upper bound 시간초과 해결)

--- java/BOJ/src/BOJ_1253_좋다.java
+++ java/BOJ/src/BOJ_1253_좋다.java
@@ -5,8 +5,6 @@ import java.util.StringTokenizer;
 
 public class BOJ_1253_좋다 {
 
-    static int ans = 0;
-
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -23,39 +21,73 @@ public class BOJ_1253_좋다 {
 
         Arrays.sort(list);
 
+        boolean[] visit = new boolean[n];
+        int ans = 0;
+
         for(int i=0; i<n; i++){
-            find(list, i, n);
+            for(int j=i+1; j<n; j++){
+                if(i == j) continue;
+
+                int leftIndex = findLeftIndex(list, list[i] + list[j], n-1);
+                int rightIndex = findRightIndex(list, list[i] + list[j], n-1);
+
+                if(leftIndex != 2000000001 && rightIndex != 2000000001) {
+                    for(int k=leftIndex; k<=rightIndex; k++){
+                        if(k != i && k != j && visit[k] == false) {
+                            ans++;
+                            visit[k] = true;
+                        }
+                    }
+                }
+            }
         }
+
         System.out.println(ans);
     }
 
-    public static void find(int[] list, int targetIndex, int size) {
-        int target = list[targetIndex];
+    public static int findLeftIndex(int[] list, int target, int size) {
         int left = 0;
-        int right = size - 1;
+        int right = size;
+        int mid;
 
-        while (left < right) {
-            // targetIndex 제외
-            if (left == targetIndex) {
-                left++;
-                continue;
+        int result = 2000000001;
+        while(left <= right) {
+            mid = (left + right) / 2;
+            if(list[mid] == target) {
+                result = mid;
+                right = mid - 1;
             }
-            if (right == targetIndex) {
-                right--;
-                continue;
+            if (list[mid] < target) {
+                left = mid + 1;
             }
-
-            int sum = list[left] + list[right];
-
-            if (sum == target) {
-                ans++;
-                return; // 중복 방지: 첫 번째 발견 후 종료
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+            if (list[mid] > target) {
+                right = mid - 1;
             }
         }
+        return result;
+    }
+
+    public static int findRightIndex(int[] list, int target, int size) {
+        int left = 0;
+        int right = size;
+        int mid;
+
+        int result = 2000000001;
+        while(left <= right) {
+            mid = (left + right) / 2;
+            if(list[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            }
+            if(list[mid] < target) {
+                left = mid + 1;
+            }
+            if(list[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return result;
     }
 
 }
